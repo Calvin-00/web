a89ab75e91d487b45e26707b1bbe0f986cff6e84
Calvin Prince <calvinprince00@gmail.com>
2023-04-08 09:43:23 +0200

    Spring MVC

--- src/main/java/com/internet/web/model/Club.java
+++ src/main/java/com/internet/web/model/Club.java
@@ -1,9 +1,6 @@
 package com.internet.web.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,7 +15,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 @Entity
-public class club {
+public class Club {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
