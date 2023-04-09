96bbbd78228560bcfb5160784a5caa438419e227
Calvin Prince <calvinprince00@gmail.com>
2023-04-09 13:58:53 +0200

    Spring MVC

--- src/main/java/com/internet/web/controller/CubController.java
+++ src/main/java/com/internet/web/controller/CubController.java
@@ -1,10 +1,16 @@
 package com.internet.web.controller;
 
 import com.internet.web.dto.ClubDto;
+import com.internet.web.model.Club;
 import com.internet.web.service.ClubService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -21,5 +27,50 @@ public class CubController {
         List<ClubDto> clubs = clubService.findAllClubs();
         model.addAttribute("clubs",clubs);
         return "clubs-list";
+    }
+
+    @GetMapping("/clubs/{clubId}")
+    public String clubDetail(@PathVariable("clubId") long clubId, Model model){
+        ClubDto clubDto = clubService.findClubById(clubId);
+        model.addAttribute("club", clubDto);
+        return "clubs-detail";
+    }
+
+    @GetMapping("/clubs/new")
+    public String createClubForm(Model model) {
+        Club club = new Club();
+        model.addAttribute("club", club);
+        return "clubs-create";
+    }
+
+    @PostMapping("/clubs/new")
+    public String saveClub(@Valid @ModelAttribute("club") ClubDto clubDto,
+                           BindingResult result,
+                           Model model) {
+        if(result.hasErrors()){
+            model.addAttribute("club", clubDto);
+            return "clubs-create";
+        }
+        clubService.saveClub(clubDto);
+        return "redirect:/clubs";
+    }
+
+    @GetMapping("/clubs/{clubId}/update")
+    public String updateClubForm(@PathVariable("clubId") long clubId, Model model) {
+        ClubDto club = clubService.findClubById(clubId);
+        model.addAttribute("club",club);
+        return "clubs-update";
+    }
+
+    @PostMapping("/clubs/{clubId}/update")
+    public String updateClub(@PathVariable("clubId") long clubId,
+                             @Valid @ModelAttribute("club") ClubDto club,
+                             BindingResult result) {
+        if(result.hasErrors()){
+            return "clubs-update";
+        }
+        club.setId(clubId);
+        clubService.updateClub(club);
+        return "redirect:/clubs";
     }
 }
--- src/main/java/com/internet/web/service/ClubService.java
+++ src/main/java/com/internet/web/service/ClubService.java
@@ -1,9 +1,15 @@
 package com.internet.web.service;
 
 import com.internet.web.dto.ClubDto;
+import com.internet.web.model.Club;
 
 import java.util.List;
 
 public interface ClubService {
     List<ClubDto> findAllClubs();
+    Club saveClub(ClubDto clubDto);
+
+    ClubDto findClubById(long clubId);
+
+    void updateClub(ClubDto club);
 }
